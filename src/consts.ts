cdc9d9fd60411bb86b48845bf8ca7fcaac0f6242
Tinnaphat Tin Somsang (Tinarskii) <tinvv@outlook.co.th>
2023-07-17 21:24:43 +0700

    🤡 Good enough

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,192 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "My personal website.";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const about = {
+  name: "Tinnaphat Somsang",
+  nick: "Tin",
+  desc: "I'm a student at Assumption Samutprakarn School. I love learning and teaching others. I enjoy math and science, but also love to code. I've been programming since I was 12 years old, and have been developing websites since then. I started contributing to open-source communities and have made some of the most popular sites on the web such as Wrong-Lang. My goal is to make programming accessible to everyone — not just those who are already good at it!",
+  dob: 1226282400000,
+  age: new Date(new Date().getTime() - 1226282400000).getFullYear() - 1970,
+  aka: "Tinarskii",
+  loc: "Samutprakarn, Thailand",
+  language: "Thai (Native), English (Fluent), Chinese (Intermediate)",
+  jobs: "Student",
+};
+
+export const contact = [
+  "Github ",
+  "github.com/tinarskii",
+  "Facebook ",
+  "facebook.com/tinarskii",
+  "Twitter",
+  "twitter.com/tinarskii",
+  "Twitch",
+  "twitch.tv/tinarskii",
+  "Youtube",
+  "youtube.com/tinetc.",
+  "Steam",
+  "steamcommunity.com/id/tinarskii",
+  "Reddit",
+  "reddit.com/u/tinarskii",
+  "Chess.com",
+  "chess.com/member/warechu",
+  "Medium",
+  "tinarskii.medium.com/#/tinarskii",
+  "Mastodon",
+  "mastodon.in.th/@tin/#/@tin@mastodon.in.th",
+  "TikTok",
+  "tiktok.com/@tinarskii/#/tinarskii",
+  "Instagram",
+  "instagram.com/t1.narskii",
+  "Discord",
+  "tinyurl.com/tinarskii/#/@acsp",
+  "Website",
+  "tinarskii.com",
+  "Blog (Hashnode)",
+  "journey.tinarskii.com",
+  "Email",
+  "tinarskii@tinarskii.com",
+  "Telephone",
+  "+66984176673",
+];
+
+export const education = [
+  {
+    name: "Assumption College Samutprakarn",
+    level: "Primary School",
+    grade: "3.92",
+    prog: "Modern Language Program",
+    grad: "April 9, 2021",
+  },
+  {
+    name: "Assumption College Samutprakarn",
+    level: "Secondary School",
+    grade: "3.90 (Secondary 2)",
+    prog: "i-Science (ACSP GEMS Project: i-Classroom)",
+  },
+];
+
+export const events = [
+  {
+    name: "Code In The Wind",
+    org: "showdown.space",
+    roles: "Participant",
+    info: "Game show where contestants implement a web page according to the reference screenshot and a set of objectives using Tailwind CSS",
+    award: "(None)",
+    links: "https://www.twitch.tv/videos/1490280508",
+    img: "https://assets.creatorsgarten.org/events/wind.png",
+  },
+  {
+    name: "Stupid Hackathon in Thailand 6 (SHiT๖)",
+    org: "Creatorsgarten Thailand",
+    roles: "Participant (Teams)",
+    info: "The Stupid Hackathon in Thailand is a collaborative hackathon in Thailand with non-competitive intentions and no focuses on business value",
+    award: "3rd MOST IMPRESSIVE TECHNOLOGY AWARD",
+    links: "https://www.youtube.com/watch?v=4Ekgdku2pVY",
+    img: "https://assets.creatorsgarten.org/events/sht6.png",
+  },
+  {
+    name: "Bkk.js Meetup #16",
+    org: "Creatorsgarten Thailand",
+    roles: "Participant",
+    info: "เพื่อเฉลิมฉลองเดือน บางกอกวิทยา ของกรุงเทพมหานคร กลัวมาแล้วกับ Bkk.js Meetup #16",
+    award: "(NOT A COMPETITION)",
+    links: "",
+    img: "https://p-u.popcdn.net/event_details/posters/000/013/479/large/715f8dc35b5366bf025ae729b7bae609701080d9.png?1660276121",
+  },
+  {
+    name: "Code Golf Party #1",
+    org: "showdown.space and Creatorsgarten Thailand",
+    roles: "Participant",
+    info: "⛳️Code Golf เป็นกิจกรรมนันทนาการสำหรับโปรแกรมเมอร์ เป็นกิจกรรมที่มีโจทย์ปัญหามาให้เขียนโปรแกรมแก้ แล้วมาหาวิธีแก้ที่ใช้ซอร์ซโค้ดขนาดเล็กที่สุด",
+    award: "(None)",
+    links: "https://www.facebook.com/showdown.space/videos/589192172698870",
+    img: "https://showdown.space/assets/banner.08ca525b.jpg",
+  },
+  {
+    name: "Hacktoberfest Thailand 2022",
+    org: "Creatorsgarten Thailand and Thinc.",
+    roles: "Contributors",
+    info: "We are hosting the Thailand chapter of the annual Hacktoberfest event, a global open-source contribution challenge held each year in October to introduce and incentivize people to contribute to open-source software.",
+    award: "(NOT A COMPETITION)",
+    links: "",
+    img: "https://miro.medium.com/max/828/1*0DQlVH0bYOGG_UEhrOMN0g.png",
+  },
+  {
+    name: "Stupid Hackathon in Thailand 7 (SHiT 7)",
+    org: "Creatorsgarten",
+    roles: "Participant (Teams)",
+    info: " The Stupid Hackathon in Thailand เป็น hackathon ที่ไม่ได้มีจุดประสงค์ในเชิงธุรกิจหรือการแข่งขัน ความตั้งใจหลักคือเราอยากให้เหล่า makers ได้มาสนุกและปล่อยของผ่าน project บ้า ๆ บอ ๆ ที่ไม่มีใครอยากได้ ไม่เน้นเชิงธุรกิจว่า product นี้จะใช้งานได้จริงไหม โดยเน้นให้ทุกคนได้มาร่วมสนุกด้วยกันก็พอ ",
+    award: "(None)",
+    links: "",
+    img: "https://assets.creatorsgarten.org/events/sht7.png",
+  },
+];
+
+export const projects = [
+  {
+    name: "WrongLang: Web Application",
+    from: "Wrong-Lang",
+    info: "The Thai Mutation Language To Normal Thai Language Global Translator",
+    gh: "github.com/wrong-lang/WrongLang",
+    link: "wl.tinarskii.com",
+    img: "/cover/wrong-lang.png",
+    full: true,
+  },
+  {
+    name: "Muk Pak Pak",
+    from: "Tinarskii",
+    info: "Collections of Thai Jokes Website",
+    gh: "github.com/tinarskii/mukpakpak",
+    link: "mukpakpak.vercel.app",
+    img: "/cover/mukpakpak.png",
+    full: true,
+  },
+];
+
+export const technology = {
+  lang: ["TypeScript", "JavaScript"],
+  framework: ["Nuxt", "Solid.js", "Svelte", "Vue", "TailwindCSS"],
+  ides: ["IntelliJ IDEA Ultimate"],
+  tools: ["Git", "OpenGPT", "GitHub Copilot"],
+  misc: [
+    "Microsoft Azure",
+    "AWS",
+    "Vercel",
+    "Cloudflare",
+    "Railway",
+    "Supabase",
+  ],
+};
+
+export const interests = {
+  subject: ["Computer Science", "Biology", "Sleeping", "Etymology"],
+  games: [
+    "Counter-Strike: Russian Offensive",
+    "Neptunia Series",
+    "Block Game (AKA Minecraft)",
+    "Cunny Archive",
+  ],
+  music: ["The Beatles", "J-POP", "K-POP (Just kidding)"],
+};
+
+export const courses = [
+  {
+    name: "Introduction to Front-End Development",
+    by: "Meta",
+    link: "https://coursera.org/verify/GTU6MQWYHHWS",
+    img: "https://coursera-certificate-images.s3.amazonaws.com/GTU6MQWYHHWS",
+  },
+];
+
+export const authors = [
+  {
+    id: 1,
+    name: "Tinnaphat Somsang",
+    img: "https://avatars.githubusercontent.com/u/68820538?s=400&u=a526b635f4697a8dd1bd21db3094bac915e5b563&v=4",
+    bio: "🏫 ACSP M.3/2 (#4233) i-Science (2nd Generation) ",
+  },
+];
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,23 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+const articles = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    id: z.number(),
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    tags: z.array(z.any()),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+export const collections = { articles };
